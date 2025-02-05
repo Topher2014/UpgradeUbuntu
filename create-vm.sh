25ec4a77180703ac6e96b4054bd38af9b03f0eb6
MonetCarriere <MonetCarriere@yahoo.com>
2025-02-05 10:08:46 -0800

    added vm

--- create-vm.sh
+++ create-vm.sh
@@ -2,7 +2,7 @@ if (multipass info mythical-vm)
  then
   echo "The VM already exists"
  else
-  echo "✨🔮 Creating a mythical vm  for you. One moment... 🧙‍"
+  echo "✨🔮 Creating a mythical vm  for you. One moment...🪄🧙‍"
   multipass launch core18 --name mythical-vm
   multipass info mythical-vm
 fi
