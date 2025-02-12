6f61e7be984e092197b80c16fe2ac0da9771592c
MonetCarriere <MonetCarriere@yahoo.com>
2025-02-12 12:27:18 -0800

    added conditional 1{

--- UpgradeUbuntu.sh
+++ UpgradeUbuntu.sh
@@ -1,5 +1,13 @@
 1. # packages should be updated
 sudo apt update
 
+if sudo apt update
+then
+  echo "✅ apt update successful!"
+else
+  echo "❌ apt update failed"
+fi
+
+
 2. # packages should be installed
 sudo apt upgrade
