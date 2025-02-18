84e8db7ce4678201df14ad9da3669d9c4b029a9d
jlotto8 <jlotto8@gmail.com>
2025-02-18 12:11:54 -0500

    Add script to updgrade and reboot, WIP.

---
+++ update-script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# The newest version should exist and the package list should be updated
+if grep -q 'VERSION_ID="22.04"' /etc/os-release  
+then  
+  echo "Ubuntu version is already 22.04"
+else  
+  echo "Ubuntu version is lower than required (22.04). Updating system"  
+  sudo apt update && sudo apt upgrade -y
+fi  
+
+# Check if a reboot is required
+if [ ! -f /var/run/reboot-required ]  
+then  
+  echo "No reboot required"
+else  
+  echo "System upgrade completed. Rebooting now"
+  sudo reboot
+fi
+
