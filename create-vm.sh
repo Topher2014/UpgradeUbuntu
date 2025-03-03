b0e21cc076705947a5bcdd752de3cd5e32771d7a
jlotto8 <jlotto8@gmail.com>
2025-03-03 12:25:41 -0500

    Add create-vm with more resources.

--- create-vm.sh
+++ create-vm.sh
@@ -1,8 +1,9 @@
+
 if (multipass info magical-vm)
  then
   echo "The VM already exists"
  else
   echo "✨‍‍🧙‍ Creating a magical vm  for you. One moment...🪄✨‍‍"
-  multipass launch 20.04 --name magical-vm
+  multipass launch 20.04 --name magical-vm --mem 2G --disk 10G --cpus 2
   multipass info magical-vm
 fi
