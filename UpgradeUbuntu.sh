9054bb19c97fb73b83b4bca2debe5536538cf864
jlotto8 <jlotto8@gmail.com>
2025-03-04 21:35:01 -0500

    Add semi-working script.

--- UpgradeUbuntu.sh
+++ UpgradeUbuntu.sh
@@ -1,48 +1,47 @@
 #!/bin/bash
 
 # Ensure the script is scheduled in cron
-if  crontab -l | grep  "@reboot /home/ubuntu/UpgradeUbuntu.sh"  
-if crontab -l | grep -q "@reboot /home/ubuntu/UpgradeUbuntu.sh"  
-then  
+if crontab -l | grep -q "@reboot /home/ubuntu/UpgradeUbuntu.sh"
+then
   echo "Cron job is scheduled"
 else
   echo "Adding script to cron for auto-execution after reboot"
   (crontab -l; echo "@reboot /home/ubuntu/UpgradeUbuntu.sh") | crontab -
 fi
- 
+
 # The newest version should exist and the package list should be updated
-if sudo apt list --upgradable | grep 'upgradable' && grep 'VERSION_ID="' /etc/os-release  
-then  
+if [ $(sudo apt list --upgradable | grep -c 'upgradable') -eq 0 ]
+then
   echo "No updates needed. System is up to date"
-  grep 'VERSION_ID="' /etc/os-release
-else  
+  grep "VERSION_ID=" /etc/os-release
+else
   echo "Updates available or system version not detected. Running apt update and upgrade"
   sudo apt update && sudo apt upgrade -y
-fi  
+fi
 
 # Check if a reboot is required before upgrading Ubuntu
-if [ ! -f /var/run/reboot-required ]  
-then  
+if [ ! -f /var/run/reboot-required ]
+then
   echo "No reboot required before upgrading Ubuntu"
-else  
+else
   echo "Reboot required before upgrading Ubuntu. Rebooting now"
   sudo reboot
-fi  
+fi
 
 # Check if a new Ubuntu release is available
-if ! sudo do-release-upgrade --check-dist | grep "New release"  
-then  
+if ! sudo do-release-upgrade --check-dist | grep -q "New release"
+then
   echo "No new Ubuntu release available"
-else  
+else
   echo "A new Ubuntu release is available. Running upgrade"
   sudo do-release-upgrade -f DistUpgradeViewNonInteractive
-fi  
+fi
 
-#commented out for now
 # Check if a reboot is required after the upgrade
-#if [ ! -f /var/run/reboot-required ]  
-#then  
- # echo "No reboot required after Ubuntu upgrade"
-#else  
- # echo "Reboot required to complete the upgrade. Rebooting now"
-#fi 
+if [ ! -f /var/run/reboot-required ]
+then
+  echo "No reboot required after Ubuntu upgrade"
+else
+  echo "Reboot required to complete the upgrade. Rebooting now"
+  sudo reboot
+fi
